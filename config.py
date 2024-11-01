120af5e1324a130673722635b8e041df677894f1
CarolinHau <119866355+CarolinHau@users.noreply.github.com>
2024-11-01 16:04:44 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Number of districts to be used for the initial solution. 
+number_of_districts = 8
+
+# Number of districts that the final solution should have at the end. 
+final_number_of_districts = 8
+
+# Maximum number of iterations for the iterative improvement process
+max_iterations = 50
+
+# Terminates the iterative improvement process if no improvement has been achieved in x consecutive iterations
+no_improvement_limit = 300
